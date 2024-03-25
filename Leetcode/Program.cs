78625452f19edafee8fbb63b5d85a4a8233b7dbf
Kesiu <kesiu.pro@gmail.com>
2024-03-25 23:58:11 +0100

    Task 1

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Leetcode.Solutions._1;
+Console.WriteLine("Type question number");
+int questionNumber = Convert.ToInt32(Console.ReadLine());
+
+switch (questionNumber)
+{
+	case 1:
+		Console.WriteLine(new Solution().TwoSum_BruteForce([2, 7, 11, 15], 9));
+		break;
+	default:
+		Console.WriteLine($"Question with number = {questionNumber} not found.");
+		break;
+}
---
+++ Leetcode/Solutions/1/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.Solutions._1
+{
+	internal class Solution
+	{
+		public int[] TwoSum_BruteForce(int[] nums, int target)
+		{
+			int i = 0;
+			int[] result = new int[2];
+			foreach (int number in nums)
+			{
+				int j = 0;
+				foreach (int number2 in nums)
+				{
+					if (i == j)
+					{
+						j++;
+						continue;
+					}
+
+					if (number + number2 == target)
+					{
+						result[0] = i;
+						result[1] = j;
+
+						return result;
+					}
+					j++;
+				}
+				i++;
+			}
+
+			return [];
+		}
+	}
+}
