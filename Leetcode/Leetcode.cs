7cc5fd449a90ce8e05a88c0c2c22f3cfc4fbd651
Kesiu <kesiu.pro@gmail.com>
2024-03-26 17:45:42 +0100

    Task 1 - additional algorithms

---
+++ Leetcode/Leetcode.cs
@@ -0,0 +1,24 @@
+﻿using Leetcode.Solutions._1;
+
+namespace Leetcode
+{
+	internal class Leetcode
+	{
+		public void ExecSolution(int questionNumber)
+		{
+			switch (questionNumber)
+			{
+				case 1:
+					Solution solution = new();
+					Console.WriteLine(string.Join(", ", solution.TwoSum_BruteForce(solution.Nums1, 9)));
+					Console.WriteLine(string.Join(", ", solution.TwoSum_TwoPass(solution.Nums2, 0)));
+					Console.WriteLine(string.Join(", ", solution.TwoSum_OnePass(solution.Nums3, 6)));
+					Console.WriteLine(string.Join(", ", solution.TwoSum_TwoPass(solution.Nums4, 19999)));
+					break;
+				default:
+					Console.WriteLine($"Question with number = {questionNumber} not found.");
+					break;
+			}
+		}
+	}
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,13 +1,4 @@
-﻿using Leetcode.Solutions._1;
-Console.WriteLine("Type question number");
+﻿Console.WriteLine("Type question number");
 int questionNumber = Convert.ToInt32(Console.ReadLine());
 
-switch (questionNumber)
-{
-	case 1:
-		Console.WriteLine(new Solution().TwoSum_BruteForce([2, 7, 11, 15], 9));
-		break;
-	default:
-		Console.WriteLine($"Question with number = {questionNumber} not found.");
-		break;
-}
+new Leetcode.Leetcode().ExecSolution(questionNumber);
