45fcb2b0374cfb01546f636db9a0d5f3acbf1201
Kesiu <kesiu.pro@gmail.com>
2024-03-28 23:53:56 +0100

    Task 2 - setup

--- Leetcode/Leetcode.cs
+++ Leetcode/Leetcode.cs
@@ -1,6 +1,4 @@
-﻿using Leetcode.Solutions._1;
-
-namespace Leetcode
+﻿namespace Leetcode
 {
 	internal class Leetcode
 	{
@@ -9,11 +7,17 @@ namespace Leetcode
 			switch (questionNumber)
 			{
 				case 1:
-					Solution solution = new();
-					Console.WriteLine(string.Join(", ", solution.TwoSum_BruteForce(solution.Nums1, 9)));
-					Console.WriteLine(string.Join(", ", solution.TwoSum_TwoPass(solution.Nums2, 0)));
-					Console.WriteLine(string.Join(", ", solution.TwoSum_OnePass(solution.Nums3, 6)));
-					Console.WriteLine(string.Join(", ", solution.TwoSum_TwoPass(solution.Nums4, 19999)));
+					Solutions._1.Solution solution1 = new();
+					Console.WriteLine(string.Join(", ", solution1.TwoSum_BruteForce(solution1.Nums1, 9)));
+					Console.WriteLine(string.Join(", ", solution1.TwoSum_TwoPass(solution1.Nums2, 0)));
+					Console.WriteLine(string.Join(", ", solution1.TwoSum_OnePass(solution1.Nums3, 6)));
+					Console.WriteLine(string.Join(", ", solution1.TwoSum_TwoPass(solution1.Nums4, 19999)));
+					break;
+				case 2:
+					Solutions._2.Solution solution2 = new();
+					solution2.AddTwoNumbers(solution2.Param1_1, solution2.Param1_2);
+					solution2.AddTwoNumbers(solution2.Param2_1, solution2.Param2_2);
+					solution2.AddTwoNumbers(solution2.Param3_1, solution2.Param3_2);
 					break;
 				default:
 					Console.WriteLine($"Question with number = {questionNumber} not found.");
---
+++ Leetcode/Solutions/2/Solution.cs
@@ -0,0 +1,119 @@
+﻿namespace Leetcode.Solutions._2
+{
+	internal class Solution
+	{
+		public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+		{
+			return l1;
+		}
+
+		#region Param1
+		public ListNode Param1_1 { get; set; } = new ListNode()
+		{
+			Value = 2,
+			Next = new ListNode()
+			{
+				Value = 4,
+				Next = new ListNode()
+				{
+					Value = 3,
+					Next = null
+				}
+			}
+		};
+
+		public ListNode Param1_2 { get; set; } = new ListNode()
+		{
+			Value = 5,
+			Next = new ListNode()
+			{
+				Value = 6,
+				Next = new ListNode()
+				{
+					Value = 4,
+					Next = null
+				}
+			}
+		};
+		#endregion
+
+		#region Param2
+		public ListNode Param2_1 { get; set; } = new ListNode()
+		{
+			Value = 0,
+			Next = null
+		};
+
+		public ListNode Param2_2 { get; set; } = new ListNode()
+		{
+			Value = 0,
+			Next = null
+		};
+		#endregion
+
+		#region Param3
+		public ListNode Param3_1 { get; set; } = new ListNode()
+		{
+			Value = 9,
+			Next = new ListNode()
+			{
+				Value = 9,
+				Next = new ListNode()
+				{
+					Value = 9,
+					Next = new ListNode()
+					{
+						Value = 9,
+						Next = new ListNode()
+						{
+							Value = 9,
+							Next = new ListNode()
+							{
+								Value = 9,
+								Next = new ListNode()
+								{
+									Value = 9,
+									Next = null
+								}
+							}
+						}
+					}
+				}
+			}
+		};
+
+		public ListNode Param3_2 { get; set; } = new ListNode()
+		{
+			Value = 9,
+			Next = new ListNode()
+			{
+				Value = 9,
+				Next = new ListNode()
+				{
+					Value = 9,
+					Next = new ListNode()
+					{
+						Value = 9,
+						Next = null
+					}
+				}
+			}
+		};
+		#endregion
+	}
+
+
+	//Definition for singly-linked list.
+	public class ListNode
+	{
+		public ListNode(int val = 0, ListNode? next = null)
+		{
+			this.Value = val;
+			this.Next = next;
+		}
+
+		public int Value { get; set; }
+
+		public ListNode? Next { get; set; }
+	}
+}
